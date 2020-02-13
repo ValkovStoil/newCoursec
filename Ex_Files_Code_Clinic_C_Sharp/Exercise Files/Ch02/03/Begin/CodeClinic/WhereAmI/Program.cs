6e4add7112eb3c6807535b3d687f150a943dbecf
ValkovStoil <valkovstoil@abv.bg>
2020-02-13 19:51:54 +0200

    Problem 2: Whre Am I?

--- Ex_Files_Code_Clinic_C_Sharp/Exercise Files/Ch02/03/Begin/CodeClinic/WhereAmI/Program.cs
+++ Ex_Files_Code_Clinic_C_Sharp/Exercise Files/Ch02/03/Begin/CodeClinic/WhereAmI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Device.Location;
 
 namespace WhereAmI
 {
@@ -8,11 +9,30 @@ namespace WhereAmI
         {
             Console.WriteLine("Starting GeoCoordinate Watcher...");
 
-            // 1. Add a Reference to System.Device.dll
-
             // 2. Use the GeoCoordinate Watcher
 
-            // 3. Use the Map Image REST API
+            var watcher = new GeoCoordinateWatcher();
+
+            watcher.StatusChanged += (s, e) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanged:{e.Status}");
+            };
+
+            watcher.PositionChanged += (s, e) =>
+             {
+                 watcher.Stop();
+
+                 Console.WriteLine($"GeoCoordinateWatcher:PositionChanged:{e.Position.Location}");
+
+                 // 3. Use the Map Image REST API
+
+                 MapImage.Show(e.Position.Location);
+             };
+
+            watcher.MovementThreshold = 100;
+
+            watcher.Start();
+
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
