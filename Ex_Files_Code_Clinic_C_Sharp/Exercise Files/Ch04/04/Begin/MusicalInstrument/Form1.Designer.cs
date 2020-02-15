26c79c1e60b8952eb610e83731b573e4c97595da
ValkovStoil <valkovstoil@abv.bg>
2020-02-15 17:48:49 +0200

    Problem 4: Musical Instrument

--- Ex_Files_Code_Clinic_C_Sharp/Exercise Files/Ch04/04/Begin/MusicalInstrument/Form1.Designer.cs
+++ Ex_Files_Code_Clinic_C_Sharp/Exercise Files/Ch04/04/Begin/MusicalInstrument/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.trackVolume = new System.Windows.Forms.TrackBar();
             this.trackFrequency = new System.Windows.Forms.TrackBar();
+            this.panel = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackFrequency)).BeginInit();
             this.SuspendLayout();
@@ -38,35 +39,50 @@
             // 
             this.trackVolume.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackVolume.Location = new System.Drawing.Point(24, 756);
-            this.trackVolume.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.trackVolume.Location = new System.Drawing.Point(16, 484);
+            this.trackVolume.Margin = new System.Windows.Forms.Padding(4);
             this.trackVolume.Maximum = 100;
             this.trackVolume.Name = "trackVolume";
-            this.trackVolume.Size = new System.Drawing.Size(1552, 90);
+            this.trackVolume.Size = new System.Drawing.Size(1031, 56);
             this.trackVolume.TabIndex = 0;
+            this.trackVolume.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TheMouseDown);
+            this.trackVolume.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TheMouseUp);
             // 
             // trackFrequency
             // 
             this.trackFrequency.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackFrequency.Location = new System.Drawing.Point(1486, 23);
-            this.trackFrequency.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.trackFrequency.Location = new System.Drawing.Point(991, 15);
+            this.trackFrequency.Margin = new System.Windows.Forms.Padding(4);
             this.trackFrequency.Maximum = 1000;
             this.trackFrequency.Minimum = 100;
             this.trackFrequency.Name = "trackFrequency";
             this.trackFrequency.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackFrequency.Size = new System.Drawing.Size(90, 721);
+            this.trackFrequency.Size = new System.Drawing.Size(56, 461);
             this.trackFrequency.TabIndex = 1;
             this.trackFrequency.Value = 100;
+            this.trackFrequency.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TheMouseDown);
+            this.trackFrequency.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TheMouseUp);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 15);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(972, 461);
+            this.panel.TabIndex = 2;
+            this.panel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TheMouseDown);
+            this.panel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel_MouseMove);
+            this.panel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TheMouseUp);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.panel);
             this.Controls.Add(this.trackFrequency);
             this.Controls.Add(this.trackVolume);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Musical Instrument";
             ((System.ComponentModel.ISupportInitialize)(this.trackVolume)).EndInit();
@@ -80,6 +96,7 @@
 
         private System.Windows.Forms.TrackBar trackVolume;
         private System.Windows.Forms.TrackBar trackFrequency;
+        private System.Windows.Forms.Panel panel;
     }
 }
 
