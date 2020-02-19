2e6fa0a41025df9a156d11a34a14a83eafd65706
ValkovStoil <valkovstoil@abv.bg>
2020-02-19 16:39:13 +0200

    LikedList

--- Ex_Files_Learning_C_Sharp_Algorithms/Exercise Files/Ch02/02_02/Begin/LinkedList/Program.cs
+++ Ex_Files_Learning_C_Sharp_Algorithms/Exercise Files/Ch02/02_02/Begin/LinkedList/Program.cs
@@ -10,7 +10,71 @@ namespace SinglyLinkedList
     {
         static void Main(string[] args)
         {
-            //TODO
+            var myList = new SinglyLinkedList();
+            myList.insertFirst(100);
+            myList.insertFirst(50);
+            myList.insertFirst(67);
+            myList.insertFirst(21);
+            myList.insertLast(999000);
+            myList.displayList();
+
+            myList.deleteFirst();
+            myList.displayList();
+        }
+        public class SinglyLinkedList
+        {
+            private Node first;
+            public bool isEmpty()
+            {
+                return (first == null);
+            }
+            public void insertFirst(int data)
+            {
+                Node newNode = new Node();
+                newNode.data = data;
+                newNode.next = first;
+                first = newNode;
+            }
+            public Node deleteFirst()
+            {
+                Node temp = first;
+                first = first.next;
+                return temp;
+            }
+            public void displayList()
+            {
+                Console.WriteLine($"List (first -- last) ");
+                Node current = first;
+                while (current != null)
+                {
+                    current.displayNode();
+                    current = current.next;
+                }
+                Console.WriteLine();
+            }
+            public void insertLast(int data)
+            {
+                Node current = first;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                Node newNode = new Node();
+                newNode.data = data;
+                current.next = newNode;
+            }
+        }
+
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public void displayNode()
+            {
+                Console.WriteLine($"<{data}>");
+            }
         }
     }
 
