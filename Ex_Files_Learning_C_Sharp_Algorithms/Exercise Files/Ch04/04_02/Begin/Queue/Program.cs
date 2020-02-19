36f83b4c561d8c0584f938505dcb8e24a580ea63
ValkovStoil <valkovstoil@abv.bg>
2020-02-19 17:47:07 +0200

    Queue

--- Ex_Files_Learning_C_Sharp_Algorithms/Exercise Files/Ch04/04_02/Begin/Queue/Program.cs
+++ Ex_Files_Learning_C_Sharp_Algorithms/Exercise Files/Ch04/04_02/Begin/Queue/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace QueueDemo
 {
@@ -6,7 +7,87 @@ namespace QueueDemo
     {
         static void Main(string[] args)
         {
-            //TODO
+            var myQueue = new Queue(5);
+            myQueue.insert(100);
+            myQueue.insert(10);
+            myQueue.insert(20);
+            myQueue.insert(30);
+            myQueue.veiw();
+
+            Console.WriteLine($"Front of the  queue is {myQueue.peekFront()} ");
+
+            Console.WriteLine("About to remove item from queue");
+            myQueue.remove();
+
+            Console.WriteLine($"Front of the  queue is {myQueue.peekFront()} ");
+
+        }
+    }
+    public class Queue
+    {
+        private int maxSize;
+        private long[] myQueue;
+        private int front;
+        private int rear;
+        private int items;
+
+        public Queue(int size)
+        {
+            maxSize = size;
+            myQueue = new long[size];
+            front = 0;
+            rear = -1;
+            items = 0;
+        }
+        public void insert(long j)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Queue is full");
+            }
+            else
+            {
+                if (rear == maxSize - 1)
+                {
+                    rear = -1;
+                }
+                rear++;
+                myQueue[rear] = j;
+                items++;
+            }
+        }
+        public long remove()
+        {
+            long temp = myQueue[front];
+            front++;
+
+            if (front == maxSize)
+            {
+                front = 0;
+            }
+
+            return temp;
+        }
+        public long peekFront()
+        {
+            return myQueue[front];
+        }
+        public bool isEmpty()
+        {
+            return (items == 0);
+        }
+        private bool isFull()
+        {
+            return (items == maxSize);
+        }
+        public void veiw()
+        {
+            Console.Write("[");
+            foreach (var q in myQueue)
+            {
+                Console.Write($"{q} ");
+            }
+            Console.WriteLine("]");
         }
     }
 }
