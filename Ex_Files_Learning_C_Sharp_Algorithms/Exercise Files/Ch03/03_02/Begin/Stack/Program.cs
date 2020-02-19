61b0b19c7bb73d724c70b405dc94bddcf3b23971
ValkovStoil <valkovstoil@abv.bg>
2020-02-19 17:22:24 +0200

    Stack

--- Ex_Files_Learning_C_Sharp_Algorithms/Exercise Files/Ch03/03_02/Begin/Stack/Program.cs
+++ Ex_Files_Learning_C_Sharp_Algorithms/Exercise Files/Ch03/03_02/Begin/Stack/Program.cs
@@ -10,7 +10,77 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            //TODO
+            var theStack = new Stack(4);
+            theStack.push("Jon Week");
+            theStack.push("The Girl Next Door");
+            theStack.push("Matrix");
+            theStack.push("Jon Week 2");
+            theStack.push("Jon Week 3");
+
+            Console.WriteLine("======================= \nAbout the peek:");
+            Console.WriteLine($"'{theStack.peek()}' is at the top of the stack.\n");
+
+            Console.WriteLine("======================= \nThe Stack contains:\n");
+
+            while (!theStack.isEmpty())
+            {
+                string movie = theStack.pop();
+                Console.WriteLine(movie);
+            }
+        }
+
+        public class Stack
+        {
+            private int maxSize;
+            private string[] stackArray;
+            private int top;
+
+            public Stack(int size)
+            {
+                maxSize = size;
+                stackArray = new string[maxSize];
+                top = -1;
+            }
+
+            public void push(string m)
+            {
+                if (isFUll())
+                {
+                    Console.WriteLine("This stack is full");
+                }
+                else
+                {
+                    top++;
+                    stackArray[top] = m;
+                }
+            }
+            public string pop()
+            {
+                if (isEmpty())
+                {
+                    Console.WriteLine("This stack is empty");
+                    return "--";
+                }
+                else
+                {
+                    int old_top = top;
+                    top--;
+                    return stackArray[old_top];
+                }
+            }
+            public string peek()
+            {
+                return stackArray[top];
+            }
+            public bool isEmpty()
+            {
+                return (top == -1);
+            }
+
+            public bool isFUll()
+            {
+                return (maxSize - 1 == top);
+            }
         }
     }
 }
