37a337f8e77255040bf5a919de91c0d24f3fc57c
ValkovStoil <valkovstoil@abv.bg>
2020-02-23 15:56:24 +0200

    End

---
+++ StudentsTracker/SchoolTracker/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTracker
+{
+    class Logger
+    {
+        const string DefaultSystemName = "SchoolTracker";
+        public static void Log(string msg, string system = DefaultSystemName, int priority = 1)
+        {
+            Console.WriteLine($"System: {system}, Priority: {priority}, Msg: {msg}");
+        }
+    }
+}
---
+++ StudentsTracker/SchoolTracker/PayRoll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolTracker
+{
+    interface IPayee
+    {
+        void Pay();
+    }
+    class PayRoll
+    {
+        List<IPayee> payees = new List<IPayee>();
+
+        public PayRoll()
+        {
+            payees.Add(new Teacher());
+            payees.Add(new Teacher());
+            payees.Add(new Principal());
+
+            Logger.Log("PayRoll started", "Payroll");
+        }
+
+        public void PayAll()
+        {
+            foreach (var payee in payees)
+            {
+                payee.Pay();
+            }
+
+            Logger.Log("PeyAll completed", "PayRoll", 2);
+        }
+    }
+}
