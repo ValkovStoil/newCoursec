a6741b9a3b943dfb3b5687eaeb2bd05a3b6a710a
ValkovStoil <valkovstoil@abv.bg>
2020-03-04 16:54:25 +0200

    Designing a confirmation view

--- ExpensesCLIENT/ExpensesClient/src/app/delete-entry/delete-entry.component.ts
+++ ExpensesCLIENT/ExpensesClient/src/app/delete-entry/delete-entry.component.ts
@@ -10,6 +10,11 @@ import { EntryService } from '../entry.service';
 })
 export class DeleteEntryComponent implements OnInit {
 
+  entry = {
+    description: '',
+    value: 0,
+    isExpense: false
+  }
   id;
 
   constructor(private route: ActivatedRoute,
@@ -17,9 +22,19 @@ export class DeleteEntryComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    this.service.getEntry(this.id).subscribe((data) => {
+    this.service.getEntry(this.id).subscribe((data:any) => {
       console.log(data);
+      this.entry.description = data.Description;
+      this.entry.isExpense = data.IsExpense;
+      this.entry.value = data.Value;
     })
   }
 
+  cancel() {
+    console.log("Cansel button was clicked");
+  }
+
+  confirm() {
+    console.log("Confirm button was clicked");
+  }
 }
