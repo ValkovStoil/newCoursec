3f1428360c871b73d06183f767d7e2dfeb8a5999
ValkovStoil <valkovstoil@abv.bg>
2020-03-05 16:08:39 +0200

    Registering users

--- ExpensesCLIENT/ExpensesClient/src/app/register/register.component.ts
+++ ExpensesCLIENT/ExpensesClient/src/app/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+
+    this.registerForm = fb.group({
+
+      userName: ['', Validators.required],
+      passowrd: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, { validator: matchingFields('passowrd', 'confirmPassword') })
+
   }
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+}
 
+function matchingFields(field1, field2) {
+  return form => {
+    if (form.control[field1].value != form.control[field2].value)
+      return { matchingFields: true }
+  }
 }
