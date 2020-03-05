4b9a40ee0019ed6036ede96c6cad5ee923af45f8
ValkovStoil <valkovstoil@abv.bg>
2020-03-05 18:31:10 +0200

    Generating a token

--- ExpensesAPI/ExpensesAPI/Controllers/AuthenticationController.cs
+++ ExpensesAPI/ExpensesAPI/Controllers/AuthenticationController.cs
@@ -1,9 +1,13 @@
 ﻿using ExpensesAPI.Models;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -26,5 +30,37 @@ namespace ExpensesAPI.Controllers
         {
             return null;
         }
+
+        private JwtPackage CreateToken(User user)
+        {
+            var tokenHanlder = new JwtSecurityTokenHandler();
+
+            var claims = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Email, user.UserName)
+            });
+
+            const string secretKey = "your secret key goes here";
+            var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(secretKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = (JwtSecurityToken)tokenHanlder.CreateJwtSecurityToken(
+                subject: claims,
+                signingCredentials: signingCredentials
+                );
+
+            var tokenString = tokenHanlder.WriteToken(token);
+
+            return new JwtPackage()
+            {
+                UserName = user.UserName,
+                Token = tokenString
+            };
+        }
     }
+}
+
+public class JwtPackage
+{
+    public string Token { get; set; }
+    public string UserName { get; set; }
 }
